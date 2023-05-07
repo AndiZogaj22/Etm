8fae746be70222b47311afbee5227a5e0c8e2c4f
AndiZogaj22 <az51520@ubt-uni.net>
2023-05-07 21:52:03 +0200

    components for footer and searchbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar';
-
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -7,6 +7,7 @@ function App() {
      <>
     <Navbar />
     hello tailwind
+    <Footer />
      </>
     </div>
   );
